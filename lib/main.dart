359602b8e4196f52b2d581c5a48443f3b9f26a1d
namb1910261 <namb1910261@student.ctu.edu.vn>
2023-02-05 21:43:22 +0700

    Khoi tao

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MyShop'),
+        ),
+        body: const Center(
+          child: Text('Welcome to MyShop'),
+        ),
+      ),
+    );
+  }
+}
